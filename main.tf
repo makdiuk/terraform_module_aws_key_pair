31cfdc5cd5c7bbdebdd95bcbe55e75a951499fc2
Maksym Nekohdiuk <maksym.glosl.net>
2024-02-25 18:18:47 +0200

    Add variable tags

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "key_pair" {
   key_name   = var.key_name
   public_key = var.public_key
-  tags       = ""
+  tags       = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -7,3 +7,9 @@ variable "public_key" {
   description = "The public key material"
   type        = string
 }
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
